6b91b53bf54e7123ce9fa19ebb0338f876a740c7
KIM TAE-HYUN <immoowmoow@gmail.com>
2018-10-21 11:12:14 +0000

    자바 설정 변경
    - lsp-java 활성화

--- conf/programming/conf-meghanada.el
+++ conf/programming/conf-meghanada.el
@@ -1,24 +1,24 @@
 ;; meghanada mode setting
 
-(add-hook 'java-mode-hook
-          (lambda ()
-            ;; meghanada-mode on
-            (meghanada-mode t)
-            (flycheck-mode +1)
-            (setq tab-width 4)
-            (setq c-basic-offset 4)
-            ;; use code format
-;;            (add-hook 'before-save-hook 'meghanada-code-beautify-before-save))
-          ))
+;; (add-hook 'java-mode-hook
+;;           (lambda ()
+;;             ;; meghanada-mode on
+;;             (meghanada-mode t)
+;;             (flycheck-mode +1)
+;;             (setq tab-width 4)
+;;             (setq c-basic-offset 4)
+;;             ;; use code format
+;; ;;            (add-hook 'before-save-hook 'meghanada-code-beautify-before-save))
+;;           ))
 
-(cond ((eq system-type 'windows-nt)
-       (setq meghanada-java-path (expand-file-name "bin/java.exe" (getenv "JAVA_HOME")))
-       (setq meghanada-maven-path "mvn.cmd"))
-      (t
-       (setq meghanada-java-path "java")
-       (setq meghanada-maven-path "mvn")))
+;; (cond ((eq system-type 'windows-nt)
+;;        (setq meghanada-java-path (expand-file-name "bin/java.exe" (getenv "JAVA_HOME")))
+;;        (setq meghanada-maven-path "mvn.cmd"))
+;;       (t
+;;        (setq meghanada-java-path "java")
+;;        (setq meghanada-maven-path "mvn")))
 
-(global-set-key (kbd "C-c C-t r") 'meghanada-reference)
-(global-set-key (kbd "C-c C-t i") 'meghanada-typeinfo)
-(global-set-key (kbd "C-c C-c m") 'meghanada-exec-main)
-(global-set-key (kbd "C-c C-v s") 'meghanada-jump-symbol)
+;; (global-set-key (kbd "C-c C-t r") 'meghanada-reference)
+;; (global-set-key (kbd "C-c C-t i") 'meghanada-typeinfo)
+;; (global-set-key (kbd "C-c C-c m") 'meghanada-exec-main)
+;; (global-set-key (kbd "C-c C-v s") 'meghanada-jump-symbol)
