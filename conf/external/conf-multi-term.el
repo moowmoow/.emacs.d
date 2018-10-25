014963b87609079f35c509156b7daa54816dc726
KIM TAE-HYUN <immoowmoow@gmail.com>
2018-10-25 08:29:47 +0000

    구글 번역기 추가

--- conf/external/conf-multi-term.el
+++ conf/external/conf-multi-term.el
@@ -1,5 +1,10 @@
 
 ;; multi-term
-(global-set-key (kbd "C-c C-m o") 'multi-term)
+(global-set-key (kbd "C-c C-m t") 'multi-term)
+(global-set-key (kbd "C-c C-m o") 'multi-term-dedicated-open)
+(global-set-key (kbd "C-c C-m q") 'multi-term-dedicated-close)
+(global-set-key (kbd "C-c C-m s") 'multi-term-dedicated-select)
+(global-set-key (kbd "C-c C-m b") 'multi-term-dedicated-toggle)
 (add-hook 'term-mode-hook (lambda () (global-set-key (kbd "C-c C-m >") 'multi-term-next)))
 (add-hook 'term-mode-hook (lambda () (global-set-key (kbd "C-c C-m <") 'multi-term-prev)))
+
