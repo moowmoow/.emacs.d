af08b108f581c7f2dc045a2760fb528fafbe7b46
KIM TAE-HYUN <immoowmoow@gmail.com>
2018-11-06 15:04:42 +0000

    테마 설정 추가

--- conf/external/conf-multi-term.el
+++ conf/external/conf-multi-term.el
@@ -4,7 +4,7 @@
 (global-set-key (kbd "C-c C-m o") 'multi-term-dedicated-open)
 (global-set-key (kbd "C-c C-m q") 'multi-term-dedicated-close)
 (global-set-key (kbd "C-c C-m s") 'multi-term-dedicated-select)
-(global-set-key (kbd "C-c C-m b") 'multi-term-dedicated-toggle)
+(global-set-key (kbd "C-x M-RET") 'multi-term-dedicated-toggle)
 (add-hook 'term-mode-hook (lambda () (global-set-key (kbd "C-c C-m >") 'multi-term-next)))
 (add-hook 'term-mode-hook (lambda () (global-set-key (kbd "C-c C-m <") 'multi-term-prev)))
 
--- conf/faces/conf-theme.el
+++ conf/faces/conf-theme.el
@@ -3,15 +3,32 @@
 
 (custom-theme-set-faces
  'paganini
- `(org-block ((t (:foreground "#5fafd7" :background "#3a3a3a"))))
- `(org-block-background ((t (:foreground nil :background "#3a3a3a"))))
- `(org-block-begin-line ((t (:foreground "#eeeeee" :background "#005f87"))))
- `(org-block-end-line ((t (:foreground "#4e4e4e" :background "#3a3a3a"))))
+
+ ;; org-table setting
+ `(org-table ((t (:foreground "#eee" :background "#222"))))
+
+ ;; org-block setting
+ `(org-block ((t (:foreground "#eee" :background "#222"))))
+ `(org-block-background ((t (:foreground "#000000" :background "#000000"))))
+ `(org-block-begin-line ((t (:foreground "#000000" :background "#444" :bold t))))
+ ;; `(org-block-begin-line ((t (:foreground "#000000" :background "#daa520" :bold t))))
+ `(org-block-end-line ((t (:foreground "#000000" :background "#333" :bold t))))
 
  ;; Flycheck
  `(flycheck-error ((t (:background "#dd0000" :foreground "#eeeeee" :bold t :underline t))))
  `(flycheck-warnline ((t (:background "#ff8700" :foreground "#eeeeee" :bold t :underline t))))
  )
+
+(eval-after-load 'org
+  '(progn
+     (set-face-attribute 'org-level-1 nil :foreground "#ffd700" :background nil :bold t :height 1.0)
+     (set-face-attribute 'org-level-2 nil :foreground "#ff8c00" :background nil :bold t :height 1.0)
+     (set-face-attribute 'org-level-3 nil :foreground "#adff2f" :background nil :bold t :height 1.0)
+     (set-face-attribute 'org-level-4 nil :foreground "#00ff00" :background nil :bold t :height 1.0)
+     (set-face-attribute 'org-level-5 nil :foreground "#228b22" :background nil :bold t :height 1.0)
+     (set-face-attribute 'org-level-6 nil :foreground "#7fffd4" :background nil :bold t :height 1.0)
+     (set-face-attribute 'org-level-7 nil :foreground "#00ffff" :background nil :bold t :height 1.0)
+     (set-face-attribute 'org-level-8 nil :foreground "#00ced1" :background nil :bold t :height 1.0)))
 
 ;; theme setting
 ;;(load-theme 'monokai t)
